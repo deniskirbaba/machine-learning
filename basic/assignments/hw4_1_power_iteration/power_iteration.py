82026297bf3cf1d3193009926dcdbbe928fbe883
deniskirbaba <kirbabadenis@gmail.com>
2024-09-24 19:36:01 +0300

    fix: notes in SVD, PCA

--- basic/assignments/hw4_1_power_iteration/power_iteration.py
+++ basic/assignments/hw4_1_power_iteration/power_iteration.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
     """
-    data: np.ndarray – symmetric diagonalizable real-valued matrix
-    num_steps: int – number of power method steps
+    data: np.ndarray - symmetric diagonalizable real-valued matrix
+    num_steps: int - number of power method steps
     
     Returns:
-    eigenvalue: float – dominant eigenvalue estimation after `num_steps` steps
-    eigenvector: np.ndarray – corresponding eigenvector estimation
+    eigenvalue: float - dominant eigenvalue estimation after `num_steps` steps
+    eigenvector: np.ndarray - corresponding eigenvector estimation
     """
     assert data is not None
     assert num_steps > 0
